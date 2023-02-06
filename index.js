bcbf7f23a3c0c898b6ae694fdefb15096598f1d6
Hojayfa Rahman <hojayfarahman@gmail.com>
2023-02-06 23:27:53 +0600

    revisit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const credentials = require(`${__dirname}/credentials.json`);
 const bodyParser = require("body-parser");
 const https = require("https");
 const app = express();
@@ -7,54 +8,52 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+	res.sendFile(`${__dirname}/index.html`);
 });
 
 app.post("/", (req, res) => {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+	const email = req.body.email;
 
-  const userData = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: { FNAME: firstName, LNAME: lastName },
-      },
-    ],
-  };
+	const userData = {
+		members: [
+			{
+				email_address: email,
+				status: "subscribed",
+				merge_fields: { FNAME: firstName, LNAME: lastName },
+			},
+		],
+	};
 
-  const userDataJson = JSON.stringify(userData);
-  const apiKey = "cb20b7dc586b07818f38cb51cadf869e-us17";
-  const listID = "f694b14a63";
-  const url = `https://us17.api.mailchimp.com/3.0/lists/${listID}`;
-  const options = {
-    method: "POST",
-    auth: `harmsway:${apiKey}`,
-  };
+	const userDataJson = JSON.stringify(userData);
+	const url = `https://us17.api.mailchimp.com/3.0/lists/${credentials.listID}`;
+	const options = {
+		method: "POST",
+		auth: `harmsway:${credentials.apiKey}`,
+	};
 
-  const request = https.request(url, options, (response) => {
-    response.on("data", (data) => {
-      const errors = JSON.parse(data).errors;
+	const request = https.request(url, options, (response) => {
+		response.on("data", (data) => {
+			const errors = JSON.parse(data).errors;
 
-      if (response.statusCode === 200 && errors.length === 0) {
-        res.sendFile(`${__dirname}/done.html`);
-      } else {
-        res.sendFile(`${__dirname}/error.html`);
-        console.log(response.statusCode);
-      }
-    });
-  });
+			if (response.statusCode === 200 && errors.length === 0) {
+				res.sendFile(`${__dirname}/done.html`);
+			} else {
+				res.sendFile(`${__dirname}/error.html`);
+				console.log(response.statusCode);
+			}
+		});
+	});
 
-  request.write(userDataJson);
-  request.end();
+	request.write(userDataJson);
+	request.end();
 });
 
 app.post("/home", (req, res) => {
-  res.redirect("/");
+	res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, () =>
-  console.log(`Listening On Port ${process.env.PORT || 3000}`)
+	console.log(`Listening On Port ${process.env.PORT || 3000}`)
 );
